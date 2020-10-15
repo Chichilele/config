8a5ceb92db74314bbdbf60c354fcfb0177fd3d52
G <gauthier.castro@cfms.org.uk>
2020-10-15 14:54:05 +0100

    add instal and setup for miniconda

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-	yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [[ "$OSTYPE" != "darwin"* ]]; then
+        # not Mac OSX
+	exit 1
 fi
 
+## install home brew
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+## install tools
 yes '' | brew install zsh curl wget
 
 ## oh-my-zsh
@@ -13,5 +16,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/esc/conda-zsh-completion ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/conda-zsh-completion
 ## apply custom zshrc 
 cp .zshrc ~/.zshrc
+
+## install & setup miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+## may not work as this is run in bash and not in zsh
+eval "$(/Users/$USER/miniconda3/bin/conda shell.zsh hook)"
+zsh -c "conda init"
+zsh -c "conda config --set auto_activate_base false"
 
 echo done!
